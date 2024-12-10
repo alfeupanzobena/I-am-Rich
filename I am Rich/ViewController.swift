d22d93dde62df3bd5f6e318523de5b3d83b209a6
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-10 09:09:47 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alfeu Panzo Bena on 27/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
